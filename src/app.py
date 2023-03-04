576e87f660c5eb67484b1bb03ae157b2b15430bb
petitmi <petitmi001@gmail.com>
2023-03-04 03:12:12 -0800

    add tracks descriptions.

--- src/app.py
+++ src/app.py
@@ -176,11 +176,8 @@ def generate_page_content(page_title, year):
             size= 'popularity:Q'
             ))
         chart = (charts[0]+charts[1]+charts[2]+charts[3]).properties(
-            width=900,height=300,title="Vibe features in different strata of the charts")
-        
-        chart_describ=""
-        chart1_describ=""
-        chart2_describ=""
+            width=900,height=280,title="Vibe features in different strata of the charts")
+    
 
         hits_c1 = hits.groupby(['key','mode']).size().reset_index(name='cnt')
         chart2 = alt.Chart(hits_c1).mark_bar().encode(
@@ -204,6 +201,20 @@ def generate_page_content(page_title, year):
             y=alt.Y('duration_ms:Q')
         ).properties(width=300,height=300, title="Duration(ms)"))
         
+        chart_describ="""
+        Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. \n 
+        Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value.\n 
+        Instrumentalness predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. \n
+        Valence is a measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
+        """
+        chart1_describ="""
+        The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1.\n The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1.
+        """
+        chart2_describ="""
+        Time Signiture is an estimated time signature. The time signature (meter) is a notational convention to specify how many beats are in each bar (or measure). The time signature ranges from 3 to 7 indicating time signatures of "3/4", to "7/4".
+        Tempo is the overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.
+        Duration_ms is the duration of the track in milliseconds.
+        """
     return html.Div(
         [html.H2(page_title),
         html.Div("This is data visualization content for {} in {}:".format(page_title, year))]
